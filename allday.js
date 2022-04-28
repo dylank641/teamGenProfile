d51831ecbc04188ea5936845f1d7d906510e7b21
James Killelea <dylan.killelea@yahoo.com>
2022-04-28 16:46:34 -0500

    got most of the question functionallity working

--- allday.js
+++ allday.js
@@ -1,138 +1,210 @@
 const inquirer = require('inquirer');
 const genMark = require('./src/layout');
 const fs = require('fs');
+const ManagerClass = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+
+var manager
+var engineers = [];
+var interns = [];
 
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        name: "hello",
-        message: "Hello"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "Please enter the Managers name (Required)",
-        default: "Dylan",
-        validate: (name) => {
-          if (name && isNaN(name)) {
-            return true;
-          } else {
-            console.log(` --You must enter a valid name--`);
-            return false;
-          }
-        },
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please enter the managers ID number (Required)",
-        default: 6464,
-        validate: (idInput) => {
-          if (Number(idInput)) {
-            return true;
-          } else {
-            console.log(` --You must enter a valid employee ID--`);
-            return false;
-          }
-        },
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter the managers email address (Required)",
-        default: "dylanallday@gmail.com",
-        validate: (emailInput) => {
-          if (emailInput.includes("@")) {
-            return true;
-          } else {
-            console.log(` --You must enter a valid email address--`);
-            return false;
-          }
-        },
-    },
-    {
-      type: 'input',
-      name: 'usageInfo',
-      message: 'Please provide the usage information. (Required)',
-      validate: usageInfo => {
-        if (usageInfo) {
-          return true;
-        } else {
-          console.log('Please provide the usage information!');
-          return false;
-        }
+const managerQuestions = [{
+    type: "input",
+    name: "hello",
+    message: "Hello and welcome! Lets start by creating a manager. (Press Enter to continue)"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter the Managers name (Required)",
+    default: "Dylan",
+    validate: (name) => {
+      if (name && isNaN(name)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid name--`);
+        return false;
       }
     },
-    {
-      type: 'input',
-      name: 'conGuide',
-      message: 'Please provide the contribution guidelines. (Required)',
-      validate: conGuide => {
-        if (conGuide) {
-          return true;
-        } else {
-          console.log('Please provide the contribution guidelines!');
-          return false;
-        }
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please enter the managers ID number (Required)",
+    default: 6464,
+    validate: (idInput) => {
+      if (Number(idInput)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid employee ID--`);
+        return false;
       }
     },
-    {
-      type: 'input',
-      name: 'testInstruc',
-      message: 'Please provide the test instructions. (Required)',
-      validate: testInstruc => {
-        if (testInstruc) {
-          return true;
-        } else {
-          console.log('Please provide the test intructions!');
-          return false;
-        }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter the managers email address (Required)",
+    default: "dylanallday@gmail.com",
+    validate: (emailInput) => {
+      if (emailInput.includes("@")) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid email address--`);
+        return false;
       }
     },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please choose a Markdown License Badge. (Required)',
-      choices: ['Apache 2.0 License', 'The MIT License', 'IBM Public License Version 1.0', 'Eclipse Public License 1.0', 'Boost Software License 1.0', 'None'],
-    },
-    {
-      type: 'input',
-      name: 'gitHub',
-      message: 'Please enter your GitHub username. (Required)',
-      validate: gitHub => {
-        if (gitHub) {
-          return true;
-        } else {
-          console.log('Please provide your GitHub username!');
-          return false;
-        }
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Please provide the managers office number. (Required)',
+    default: 6969,
+    validate: usageInfo => {
+      if (Number(usageInfo)) {
+        return true;
+      } else {
+        console.log(' --Please enter a valid office number--');
+        return false;
       }
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Please provide your email addrress. (Required)',
-      validate: testInstruc => {
-        if (testInstruc) {
-          return true;
-        } else {
-          console.log('Please provide your email addrress!');
-          return false;
-        }
-      }
-    },
-  
+    }
+  },
+
 ];
+
+let engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter the Engineers name (Required)",
+    default: "Dan",
+    validate: (name) => {
+      if (name && isNaN(name)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid name--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please enter the Engineers ID number (Required)",
+    default: 1234,
+    validate: (idInput) => {
+      if (Number(idInput)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid employee ID--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter the Engineers email address (Required)",
+    default: "dan@gmail.com",
+    validate: (emailInput) => {
+      if (emailInput.includes("@")) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid email address--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please provide the Engineers Github username. (Required)',
+    default: 'daniscool',
+    validate: usageInfo => {
+      if (usageInfo) {
+        return true;
+      } else {
+        console.log(' --Please enter a github username--');
+        return false;
+      }
+    }
+  },
+]
+
+let internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter the Interns name (Required)",
+    default: "Ben",
+    validate: (name) => {
+      if (name && isNaN(name)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid name--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please enter the Interns ID number (Required)",
+    default: 6278,
+    validate: (idInput) => {
+      if (Number(idInput)) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid employee ID--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter the Interns email address (Required)",
+    default: "benthegod@gmail.com",
+    validate: (emailInput) => {
+      if (emailInput.includes("@")) {
+        return true;
+      } else {
+        console.log(` --Please enter a valid email address--`);
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please provide the Interns Github username. (Required)',
+    default: 'benRocks',
+    validate: usageInfo => {
+      if (usageInfo) {
+        return true;
+      } else {
+        console.log(' --Please enter a valid github username--');
+        return false;
+      }
+    }
+  },
+]
 
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
+function writeToFile() {
   //send data to generate markdonw
   //use fs to write it to file
+  var team = {
+    manager,
+    engineers,
+    interns
+  }
   return new Promise((resolve, reject) => {
-    fs.writeFile('README.md', genMark(data), err => {
+    fs.writeFile('./dist/index.html', genMark(team), err => {
       if (err) {
         reject(err);
         return;
@@ -148,15 +220,67 @@ function writeToFile(data) {
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions)
-.then(function(answers){
-  return writeToFile(answers)
-})
-.catch(err => {
-  console.log(err);
-});
+  inquirer.prompt(managerQuestions)
+    .then(function (answers) {
+      manager = new ManagerClass(answers.name, answers.id, answers.email, answers.officeNumber)
+      menu();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
 }
 
+function internPop() {
+  inquirer.prompt(internQuestions)
+    .then(function (answers) {
+      interns = interns.push (new (answers.name, answers.id, answers.email, answers.github))
+      menu();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+}
+
+
+function eng() {
+  inquirer.prompt(engineerQuestions)
+    .then(function (answers) {
+      engineers = engineers.push (new Engineer(answers.name, answers.id, answers.email, answers.github))
+      menu();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+}
+
+function menu() {
+  inquirer.prompt({
+      type: 'list',
+      name: 'menu',
+      message: 'Please choose an answer choice. (Required)',
+      choices: ['Enter an Engineer', 'Enter an Intern', 'Done and Generate File']
+    })
+    .then(function (answer) {
+      console.log(answer.menu);
+      if(answer.menu=="Enter an Engineer"){
+        eng();
+        
+      }
+      else if( answer.menu =="Enter an Intern"){
+        internPop();
+        
+      }
+    })
+}
+
+
+
+
 // Function call to initialize app
+
 init();
+
+//interns.push (new Inter)
